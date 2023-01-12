856ef086765b4d551d59aa6280638a0242ebfb3e
Иван Захаров <ivan@Ivans-MacBook-Air.local>
2023-01-12 11:27:31 +0300

    add fasade header

--- src/Fasade/Fasade.h
+++ src/Fasade/Fasade.h
@@ -1,16 +1,33 @@
 #ifndef FASADE_FASADE_H
 #define FASADE_FASADE_H
 
+#include <string>
+
 #include "../Parser/Parser.h"
+#include "../OGLWidget/OGLWidget.h"
+#include "../Helpers/Helpers.h"
 
-namespace s21 {
+namespace s21
+{
 
-class Fasade {
-    private:
-        Parser pars_;
-    public:
-        void Parse(std::string &file_path);
-};
+  class Fasade {
+  private:
+    Parser *Parsку;
+    OGLWidget *Widget;
+
+  public:
+    void OpenFile(std::string &file_path);
+    void MoveObject(float x, float y, float z);
+    void RotateObject(float x, float y, float z);
+    void ScaleObject(float scale);
+    void SetProjection(Projection type);
+    void SetLineType(EdgesType type);
+    void SetLineColor(QColor color);
+    void SetPointType(VerticesType type);
+    void SetPointColor(QColor color);
+    void SetBgColor(QColor color);
+    void SaveImage(RenderType type);
+  };
 
 } // namespace s21
 
