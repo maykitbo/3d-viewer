16ee2676ffd2893b20da2f9615a7ada131c06fd6
Alexey <zam7@icloud.com>
2023-01-20 20:55:52 +0100

    parser

--- src/Parser/Parser.cc
+++ src/Parser/Parser.cc
@@ -22,16 +22,17 @@ void Parser::parse(std::string &fName) {
         }
     }
     vertex_.resize(vertex_i_);
-    lines_.resize(lines_i_-1);
+    lines_.resize(lines_i_);
     file_.close();
 }
 
 void Parser::caseV() {
     if (file_.peek() == ' ') {
         for (int k = 0; k < 3; ++k) {
+            vertex_.resize(vertex_.size() + 1);
             file_ >> vertex_[vertex_i_];
             ++vertex_i_;
-            if (!(vertex_i_ % 1000)) vertex_.resize(vertex_i_ + 1001);
+            // if (!(vertex_i_ % 1000)) vertex_.resize(vertex_i_ + 1001);
         }
         // ----------------------------------------------------------------------  add [w]
     } else { //  ----------------------------------------------------------------  add vn vb
@@ -43,15 +44,15 @@ void Parser::caseF() {
     // lines.push_back(f_1_vec());
     while (file_.peek() != '\n' && file_) {
         if (file_.peek() == ' ') {
+            lines_.resize(lines_.size() + 2);
             file_ >> lines_[lines_i_];
             lines_[lines_i_]--;
-            if (lines_i_ % 2) {
+            if (lines_i_ % 2 && lines_i_) {
                 ++lines_i_;
                 lines_[lines_i_] = lines_[lines_i_ - 1];
             }
-            // lines_[lines_i_]--;
             ++lines_i_;
-            if (!(lines_i_ % 1000)) lines_.resize(lines_i_ + 1001);
+            std::cout << lines_i_ << " ";
         }
         // if (file_.peek() == ' ') file_ >> *lines_.emplace_back(1);
         else file_.ignore();  //  ------------------------------------------------  add f 1//2//3
@@ -62,8 +63,8 @@ void Parser::caseF() {
 inline void Parser::clear() {
     vertex_.clear();
     lines_.clear();
-    vertex_.resize(1001);
-    lines_.resize(1001);
+    vertex_.resize(1);
+    lines_.resize(1);
     vertex_i_ = 0, lines_i_ = 0;
 }
 
